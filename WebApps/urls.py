225520ade64c161f6f64ea6b3a7a36d2ed96d278
Kusuma Kalyan <49522429+KusumaKalyan@users.noreply.github.com>
2020-05-10 15:46:08 +0530

    Update urls.py

--- WebApps/urls.py
+++ WebApps/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('FotoEditor.urls')),
+    path('/', include('FotoEditor.urls')),
     path('admin/', admin.site.urls),
     path('fotoEditor/', include('FotoEditor.urls')),
 ]
